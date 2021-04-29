2cbe0ad47d0616f5c83c309066c4a783581e100c
Ayush singhal <68475187+SinghalAyushh@users.noreply.github.com>
2021-04-29 09:17:00 +0530

    initial commit

--- src/index.js
+++ src/index.js
@@ -3,6 +3,16 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { createStore } from 'redux';
+
+//store --globalized sate
+
+//action increment 
+
+//reducer
+
+//dispatch
+
 
 ReactDOM.render(
   <React.StrictMode>
